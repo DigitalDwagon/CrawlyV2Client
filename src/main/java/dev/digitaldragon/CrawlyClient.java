17826b2794fcf12fa814d79cc838e3155d44d004
DigitalDragon <34419970+DigitalDwagon@users.noreply.github.com>
2023-07-28 03:13:48 -0400

    Crawly version 2 client initial publishing commit

--- src/main/java/dev/digitaldragon/CrawlyClient.java
+++ src/main/java/dev/digitaldragon/CrawlyClient.java
@@ -9,60 +9,102 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+import javax.net.ssl.SSLHandshakeException;
 import java.io.IOException;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.ProtocolException;
-import java.net.URL;
+import java.net.*;
 import java.time.Instant;
 import java.util.*;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
-public class Main {
-    public static final String USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36 CrawlyProjectCrawler/0.0.4";
-    public static final String CRAWLER = "crawlyprojectofficial 0.0.4";
+public class CrawlyClient {
+    public static final String USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36 CrawlyProjectCrawler/0.0.4 (email crawlyproject@digitaldragon.dev)";
+    public static final String CRAWLER = "crawlyprojectofficial 0.0.6";
     public static final String USERNAME = "DigitalDragon";
     public static final String TRACKER_HOST = "http://localhost:4567";
+    private static final int TOTAL_URLS = 2000;
+
+    // Fixed thread pool executor service
+    private static final ExecutorService executorService = Executors.newFixedThreadPool(200);
+    private static JSONArray sendCache = new JSONArray();
 
     public static void main(String[] args) {
-
-        for (int i = 0; i < 5; i++) {
-            String url = fetchURL();
-
-            //If URL is null or empty go to next iteration.
-            if (url == null || url.isEmpty()) {
-                System.out.println("No Url Found");
-                continue;
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            if (!sendCache.isEmpty()) {
+                submitSendCache(TRACKER_HOST);
             }
+        }));
 
-            System.out.println(url);
+        try {
+            // Keep track of Futures
+            List<Future<?>> futures = new ArrayList<>();
 
-            JSONObject data = crawlPage(url);
+            while (true) {
+                // Remove completed tasks from futures list
+                futures.removeIf(Future::isDone);
 
-            //If Crawl Failed go to next iteration.
-            if (data == null) {
-                System.out.println("Crawl failed.");
-                continue;
+                if (futures.isEmpty()) {
+                    System.out.println("Running on empty.");
+                }
+
+                // If there are no tasks left, then continue fetching new URLs, else sleep
+                if (futures.size() < 200){
+                    List<String> urls = getUrlFromTracker(TOTAL_URLS, USERNAME, TRACKER_HOST);
+                    if (urls == null) {
+                        System.out.println("null, continuing");
+                        continue;
+                    }
+                    for (String url : urls) {
+                        // Submit returns a Future, keep track of these
+                        futures.add(executorService.submit(new CrawlTask(url)));
+                    }
+                }
+
+
             }
-
-            //Print crawled data
-            System.out.println("Crawled Data:");
-            System.out.println(data);
-
-            //Submit crawled data.
-            System.out.println("Submitting crawled data:");
-            sendDoneToTracker(data, TRACKER_HOST);
+        } catch (Exception e) {
+            System.out.println("An exception occurred: " + e.getMessage());
+        } finally {
+            executorService.shutdown();
         }
     }
 
-    public static String fetchURL() {
-        System.out.println("Getting URLs from Tracker:");
-        String url = getUrlFromTracker(1, USERNAME, TRACKER_HOST);
+    final static class CrawlTask implements Callable<Void> {
 
-        return url;
+        private final String url;
+
+        private CrawlTask(String url) {
+            this.url = url;
+        }
+
+        @Override
+        public Void call() {
+            if (url == null || url.isEmpty()) {
+                System.out.println("No Url Found");
+                return null;
+            }
+
+            System.out.println(url);
+            JSONObject data = crawlPage(url);
+            if (data == null) {
+                //System.out.println("Crawl failed.");
+                return null;
+            }
+
+            //System.out.println("Crawled Data:");
+            //System.out.println(data);
+
+            //System.out.println("Submitting crawled data:");
+            sendDoneToTracker(data, TRACKER_HOST);
+            System.out.println(data.get("url"));
+            return null;
+        }
     }
 
     public static JSONObject crawlPage(String url) {
-        System.out.println("Crawling:");
+        //System.out.println("Crawling:");
         return grabPage(url);
     }
 
@@ -74,7 +116,7 @@ public class Main {
      * @param trackerHost the hostname of the tracker server.
      * @return the URL fetched from the tracker, or null if an error occurs.
      */
-    public static String getUrlFromTracker(int amount, String username, String trackerHost) {
+    public static List<String> getUrlFromTracker(int amount, String username, String trackerHost) {
         try {
             String url = String.format("%s/jobs/queue?amount=%s&username=%s", trackerHost, amount, username);
             HttpURLConnection connection = (HttpURLConnection) new URL(url).openConnection();
@@ -86,7 +128,7 @@ public class Main {
             for (int i = 0; i < jsonUrls.length(); i++) {
                 urls.add(jsonUrls.getString(i));
             }
-            return urls.get(0);
+            return urls;
         } catch (IOException e) {
             System.out.println("Error fetching from tracker.");
             return null;
@@ -120,7 +162,7 @@ public class Main {
             if (document != null) {
                 data = buildJsonData(document, grabUrl);
             } else {
-                System.out.println("Unsupported content type.");
+                //System.out.println("Unsupported content type.");
                 data = null;
             }
         } catch (IOException e) {
@@ -144,8 +186,31 @@ public class Main {
                     .followRedirects(false)
                     .userAgent(USER_AGENT)
                     .ignoreHttpErrors(true)
+                    .timeout(15000) //15 seconds
                     .get();
-        } catch (UnsupportedMimeTypeException e) {
+        } catch (UnsupportedMimeTypeException | SSLHandshakeException e) {
+            //should release the claim
+            //for now does nothing
+            return null;
+        } catch (ConnectException e) {
+            //should send a 000 status to tracker
+            //for now does nothing.
+            return null;
+        } catch (SocketTimeoutException e) {
+            //should send 000
+            //does nothing for now
+            return null;
+        } catch (UnknownHostException e) {
+            //should do something idk
+            //does nothing for now
+            return null;
+        } catch (SocketException e) {
+            //should do something
+            //for now does not
+            return null;
+        } catch (IOException e) {
+            //unfortunately generic ioexceptions can appear.
+            //example: Underlying input stream returned zero bytes
             return null;
         }
     }
@@ -158,6 +223,13 @@ public class Main {
      * @return a JSON object containing the data from the web page.
      */
     public static JSONObject buildJsonData(Document document, String grabUrl) {
+        JSONObject headers = new JSONObject();
+        document.connection().response().headers().forEach(headers::put); //TODO this may overwrite headers with the same name (eg multiple Set-Cookie headers)
+
+        JSONArray discoveredOutlinks = extractAndCleanURLs(document.select("a[href]"), grabUrl);
+        discoveredOutlinks = appendLocationHeader(document, discoveredOutlinks);
+
+
         JSONObject data = new JSONObject();
         data.put("time", Instant.now());
         data.put("url", grabUrl);
@@ -165,14 +237,24 @@ public class Main {
         data.put("response", document.connection().response().statusCode());
         data.put("username", USERNAME);
         data.put("client", CRAWLER);
-
-        JSONArray discoveredOutlinks = extractAndCleanURLs(document.select("a[href]"), grabUrl);
-        discoveredOutlinks = appendLocationHeader(document, discoveredOutlinks);
-
+        data.put("headers", headers);
+        data.put("meta", getMeta(document.select("meta")).put("html_title", document.title()));
         data.put("discovered_outlinks", discoveredOutlinks);
         data.put("discovered_embeds", extractAndCleanURLs(document.select(":not(a)[href]"), grabUrl));
 
         return data;
+    }
+
+    public static JSONObject getMeta(Elements elements) {
+        JSONObject meta = new JSONObject();
+        for (Element tag : elements) {
+            String name = tag.attr("name");
+            String content = tag.attr("content");
+            if (!name.isEmpty() && !content.isEmpty()) {
+                meta.put(name, content);
+            }
+        }
+        return meta;
     }
 
     /**
@@ -204,7 +286,11 @@ public class Main {
         Set<String> urls = getURLs(elements);
 
         for (String url : urls) {
-            url = cleanUrl(url, baseUrl);
+            try {
+                url = cleanUrl(url, baseUrl);
+            } catch (MalformedURLException malformedURLException) {
+                continue;
+            }
 
             if (url != null) {
                 urlArray.put(url);
@@ -231,34 +317,44 @@ public class Main {
         return urls;
     }
 
+
     /**
      * Cleans and normalizes a URL based on the base URL.
      *
      * @param url     the URL to clean.
      * @param baseUrl the base URL used to resolve relative URLs.
      * @return the cleaned and normalized URL, or null if the URL is empty or starts with "#".
+     * @throws MalformedURLException if no protocol is specified, or an unknown protocol is found, or spec is null.
      */
-    private static String cleanUrl(String url, String baseUrl) {
+    private static String cleanUrl(String url, String baseUrl) throws MalformedURLException {
         if (url.startsWith("#") || url.isEmpty()) {
             return null;
         }
 
         if (url.startsWith("//")) {
-            url = "https:" + url;
+            return new URL("https:" + url).toExternalForm();
         }
 
-        if (url.startsWith("/") || url.startsWith("./")) {
-            url = url.replaceFirst("(^\\/|\\.\\/)","");
-            url = baseUrl + url;
+        URL baseURL = new URL(baseUrl);
+
+        // If the URL starts with a "/", it's taken as relative to the domain.
+        if (url.startsWith("/")) {
+            return new URL(baseURL.getProtocol(), baseURL.getHost(), url).toExternalForm();
         }
 
-        if (!url.startsWith("http://") && !url.startsWith("https://") && !url.contains("://")) {
-            int lastSlashIndex = baseUrl.lastIndexOf("/");
-            String domainBaseUrl = baseUrl.substring(0, lastSlashIndex + 1);
-            url = domainBaseUrl + url;
+        if (url.startsWith("./")) {
+            String fileBaseUrl = baseURL.getFile();
+            int lastSlashIndex = fileBaseUrl.lastIndexOf("/");
+            String directoryBaseUrl = fileBaseUrl.substring(0, lastSlashIndex + 1);
+            return new URL(baseURL, directoryBaseUrl + url.substring(2)).toExternalForm();
+        } else {
+            // Attempts to create a URL directly and falls back to creating a URL relative to the base URL
+            try {
+                return new URL(url).toExternalForm();
+            } catch (MalformedURLException relativeUrlEx) {
+                return new URL(baseURL, url).toExternalForm();
+            }
         }
-
-        return url;
     }
 
 
@@ -270,8 +366,25 @@ public class Main {
      */
     public static void sendDoneToTracker(JSONObject data, String trackerHost) {
         try {
-            String url = String.format("%s/jobs/submit", trackerHost);
-            String requestBody = data.toString();
+            sendCache.put(data);
+
+            if (sendCache.length() >= 5) {
+                submitSendCache(trackerHost);
+            }
+        } catch (JSONException e) {
+            System.out.println("Error parsing JSON");
+        }
+    }
+
+    public static void submitSendCache(String trackerHost) {
+        final JSONArray items = new JSONArray(sendCache);
+        sendCache.clear();
+        try {
+            JSONObject send = new JSONObject();
+            send.put("items", items);
+
+            String url = String.format("%s/jobs/submit?username=%s", trackerHost, USERNAME);
+            String requestBody = send.toString();
             HttpURLConnection connection = (HttpURLConnection) new URL(url).openConnection();
             connection.setRequestMethod("POST");
             connection.setRequestProperty("Content-Type", "application/json");
@@ -279,10 +392,11 @@ public class Main {
             connection.setDoOutput(true);
             connection.getOutputStream().write(requestBody.getBytes());
             String responseJson = readResponse(connection);
-            System.out.println("Tracker response: " + responseJson);
+            //System.out.println("Tracker response: " + responseJson);
             // Parse the JSON response
             JSONObject jsonResp = new JSONObject(responseJson);
             boolean success = jsonResp.getBoolean("success");
+
 
             if (!success) {
                 JSONArray error = jsonResp.getJSONArray("error");
@@ -293,10 +407,13 @@ public class Main {
                     }
                 }
             }
-        } catch (IOException e) {
+        } catch (IOException ioException) {
+            ioException.printStackTrace();
             System.out.println("Error submitting to tracker");
-        } catch (JSONException e) {
-            System.out.println("Error parsing JSON response");
+        } catch (JSONException jsonException) {
+            jsonException.printStackTrace();
+            System.out.println("error submitting to tracker");
         }
+
     }
 }
